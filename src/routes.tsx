d3d307e605b0aecc7d6a9efaefaa1402afe454b9
financelap <82516958+financelap@users.noreply.github.com>
2021-04-27 20:32:59 +0700

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,9 +3,9 @@ import { Route, Switch } from "react-router-dom";
 import Governance from "views/Governance/Governance";
 import Home from "views/Home/Home";
 import Learn from "views/Learn/Learn";
-import Pools from "views/Pools/Pools";
+
 import Compensation from "views/Compensation/Compensation";
-import Vaults from "views/Vault/Vaults";
+
 
 
 
@@ -13,10 +13,10 @@ function Routes() {
   return (
     <Switch>
       <Route path="/learn" component={Learn}></Route>
-      <Route path="/pools" component={Pools}></Route>
+
       <Route path="/governance" component={Governance}></Route>
       <Route path="/compensation" component={Compensation}></Route>
-      <Route path="/vault" component={Vaults}></Route>
+
       <Route path="/" component={Home}></Route>
 
     </Switch>
