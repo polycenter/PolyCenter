af2f95fe479c469d0e7347d81e721bd36e489e83
financelap <82516958+financelap@users.noreply.github.com>
2021-04-27 09:10:59 +0700

    Update Pools.tsx

--- src/views/Pools/Pools.tsx
+++ src/views/Pools/Pools.tsx
@@ -101,7 +101,7 @@ function Pools() {
             Get BACKUP
           </BuyButtonZZZ>
           <BuyButtonNAP
-            href="https://bsc.backuptoken.info/token/0x48e15ABBcb21C7f564c1B5Ae08A9f7829745F6D7"
+            href="https://exchange.backup.finance/#/swap?inputCurrency=0x0E6ef701E9CEF56A516C2e10ad093Be351B6c90f"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -114,7 +114,7 @@ function Pools() {
             rel="noopener noreferrer"
           >
             <img src={unismall} className="uni-logo" alt="uniswap" />
-            <span>Get BACKUP</span>
+            <span>Get SAFEGAS</span>
           </BuyButtonNAP>
         </BuyButtons>
         <BoxContainer color="black">
@@ -160,7 +160,7 @@ function Pools() {
                       <b>{balance?.toFixed(4)}</b>
                     </span>
                     <div className="balance-token-name">
-                      56 {name} = ${tokenPrice?.toFixed(4)}
+                      0 {name} = ${tokenPrice?.toFixed(4)}
                     </div>
                   </div>
                 );
