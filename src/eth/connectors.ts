294914865c1829c43fa7cead038f4cdcb8577fb3
financelap <82516958+financelap@users.noreply.github.com>
2021-04-28 08:21:50 +0700

    Update connectors.ts

--- src/eth/connectors.ts
+++ src/eth/connectors.ts
@@ -1,15 +1,15 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
-// import { NetworkConnector } from "@web3-react/network-connector";
+import { NetworkConnector } from "@web3-react/network-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-// import { WalletLinkConnector } from "@web3-react/walletlink-connector";
-// import { LedgerConnector } from "@web3-react/ledger-connector";
-// import { TrezorConnector } from "@web3-react/trezor-connector";
-// import { FrameConnector } from "@web3-react/frame-connector";
-// import { AuthereumConnector } from "@web3-react/authereum-connector";
-// import { FortmaticConnector } from "@web3-react/fortmatic-connector";
-// import { PortisConnector } from "@web3-react/portis-connector";
-// import { SquarelinkConnector } from "@web3-react/squarelink-connector";
-// import { TorusConnector } from "@web3-react/torus-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { LedgerConnector } from "@web3-react/ledger-connector";
+import { TrezorConnector } from "@web3-react/trezor-connector";
+import { FrameConnector } from "@web3-react/frame-connector";
+import { AuthereumConnector } from "@web3-react/authereum-connector";
+import { FortmaticConnector } from "@web3-react/fortmatic-connector";
+import { PortisConnector } from "@web3-react/portis-connector";
+ import { SquarelinkConnector } from "@web3-react/squarelink-connector";
+ import { TorusConnector } from "@web3-react/torus-connector";
 
 const POLLING_INTERVAL = 12000;
 const RPC_URLS: { [chainId: number]: string } = {
@@ -21,10 +21,10 @@ export const injected = new InjectedConnector({
   supportedChainIds: [56],
 });
 
-// export const network = new NetworkConnector({
-//   urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
-//   defaultChainId: 1,
-// });
+ export const network = new NetworkConnector({
+   urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
+   defaultChainId: 56,
+ });
 
 export const walletconnect = new WalletConnectConnector({
   rpc: { 56: RPC_URLS[56] },
